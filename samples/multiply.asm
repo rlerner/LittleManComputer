5d790e041085867cb81673d0defa39ffbaf9d307
Robert Lerner <rlerner@users.noreply.github.com>
2022-07-23 17:13:32 -0500

    Update multiply.asm

--- samples/multiply.asm
+++ samples/multiply.asm
@@ -1,22 +1,40 @@
-// Take two inputs and multiply them
-	INP
-	STA NUM1
-	INP 
-	STA NUM2
-LOOP    LDA TOTAL
-	ADD NUM1
-	STA TOTAL
-	LDA NUM2
-	SUB ONE
-	STA NUM2
-	BRP LOOP
-	LDA TOTAL
-	SUB NUM1
-	STA TOTAL
-	OUT
-	HLT
-		
-NUM1	DAT
-NUM2	DAT
-ONE	DAT 1
-TOTAL	DAT 0
+// Multiply Two Numbers - R.Lerner 2022/07/23 - Reduces cycle count by multiplying large numbers instead of small.
+
+		INP
+		STA a
+		INP
+		STA b
+		SUB a
+		BRP SWAPVAR // Some branching to figure out which has the higher var, then swaps them out to reduce cycle count
+		BRA DOMATH
+
+SWAPVAR	LDA b
+		STA swap
+		LDA a
+		STA b
+		LDA swap
+		STA a
+
+DOMATH	LDA a
+		STA output
+		LDA b
+		SUB one
+		STA b
+MULT	BRZ DONE 	// If zero is in accumulator (var B) jump to done and kick it out
+
+		LDA output
+		ADD a
+		STA output
+		LDA b 		// Set accumulator to "b" for the MULT / BRZ compare above
+		SUB one
+		STA b
+		BRA MULT 	// Jump back to multiply loop
+
+DONE	LDA output
+		OUT
+		HLT
+		a DAT
+		b DAT
+		output DAT
+		one DAT 1
+		swap DAT
